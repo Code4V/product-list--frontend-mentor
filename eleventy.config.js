e5139bdd243a7c5a99355473ae5f1856a6cd847b
Jeanne Francis Rivas <vashypies@gmail.com>
2025-04-27 17:45:45 +0800

    removal of public build folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,9 @@
-const path = require("node:path");
-const Image = require("@11ty/eleventy-img");
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
-
 module.exports = function (eleventyConfig) {
- 
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		// output image formats
 		formats: ["avif", "webp", "jpeg", "jpg"],
-
-		// output image widths
 		widths: ["auto"],
-
-		// optional, attributes assigned on <img> nodes override these values
 		htmlOptions: {
 			imgAttributes: {
 				loading: "lazy",
@@ -20,10 +11,11 @@ module.exports = function (eleventyConfig) {
 			},
 			pictureAttributes: {}
 		},
-	});
+});
 
+	eleventyConfig.addPassthroughCopy("src/assets");
 
-  eleventyConfig.addCollection("products", function (collectionsApi) {
+  eleventyConfig.addCollection("products", (collectionsApi) => {
     const allProducts = collectionsApi.getAll()[0].data.products;
     return allProducts;
   })
